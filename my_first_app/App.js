5fd8eb1f33ad0c4ca207108de809890fd89b9a2a
Braun Park <sum8179@gmail.com>
2021-03-28 11:34:40 +0900

    state

--- my_first_app/App.js
+++ my_first_app/App.js
@@ -7,15 +7,62 @@
  */
 
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 class App extends Component {
+
+  state = {
+    sampleText : 'Hello World',
+    sampleBoolean: true,
+    sampleNum: 1
+  }
+
+  inputText = () => (
+    this.state.sampleBoolean ? 
+      <Text>sampleBoolean is true</Text>
+      :
+      <Text>sampleBoolean is false</Text>
+  )
+
+  changeState = () => {
+    if(!this.state.sampleBoolean) {
+      this.setState({
+        sampleText: 'Text Changed!!',
+        sampleBoolean: true
+      })
+    }else{
+      this.setState({
+        sampleText: 'Hello World!!!',
+        sampleBoolean: false
+      })
+    }
+  }
+
+  onAdd = () => {
+    this.setState(prevState => {
+      return {
+        sampleNum: prevState.sampleNum + 1
+      }
+    })
+  }
+
   render() {
     return (
-      <View>
-        <Text>Hello World</Text>
+      <View style={styles.background}>
+        <Text onPress={this.onAdd}>
+          {this.state.sampleNum}
+        </Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
 export default App;
