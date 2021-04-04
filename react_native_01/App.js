0317dc3dc10e251dcf44b620794f10cd734ccac0
Braun Park <sum8179@gmail.com>
2021-04-04 15:01:02 +0900

    touch event

--- react_native_01/App.js
+++ react_native_01/App.js
@@ -8,21 +8,22 @@
 
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import Header from './src/header';
 
 
 class App extends Component {
+
+  state = { 
+    appName : 'My First App'
+  }
   render() {
     return (
       <View style={styles.mainView}>
-        <View style={styles.subView}>
-          <Text style={styles.mainText}>Hello World</Text>
-        </View>
-        <View style={styles.subView}>
-          <Text>Hello World</Text>
-        </View>
-        <View style={styles.anotherSubView}>
-        <Text style={styles.mainText}>Hello World</Text>
-        </View>
+        {/* <Header name={this.state.appName}/> */}
+        <Text
+          style={styles.mainView}
+          onPress={()=>alert('Text touch Event')}
+        >Hello World</Text>
       </View>
     )
   }
@@ -31,16 +32,14 @@ class App extends Component {
 const styles = StyleSheet.create({
   mainView: {
     flex:1, 
-    backgroundColor: 'green',
+    backgroundColor: 'white',
     paddingTop:50,
     alignItems: 'center',
     justifyContent: 'center'
   }, 
   subView: {
-    flex: 1,
     backgroundColor: 'yellow',
     marginBottom: 10,
-    width: '50%'
   },
   anotherSubView: {
     flex: 2,
@@ -51,8 +50,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   mainText: {
-    fontSize: 50,
-    fontWeight: 'bold',
+    fontSize: 20,
+    fontWeight: 'normal',
     color: 'red',
     padding: 20
   }
